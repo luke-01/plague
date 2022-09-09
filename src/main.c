6605bd282d8829aa84e0da8917243dd19d8d1fc9
Lucas Galindo <lanternmaze@gmail.com>
2022-09-09 13:46:14 -0400

    initial commit: glfw base code

---
+++ src/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <GLFW/glfw3.h>
+
+enum {
+	WINDOW_WIDTH = 1280,
+	WINDOW_HEIGHT = 720,
+};
+
+int main(void) {
+	if (!glfwInit()) {
+		fprintf(stderr, "failed to initialize GLFW\n");
+		return 1;
+	}
+
+	// if this is not set then glfw will attempt to create an opengl context which, for whatever 
+	// reason causes either vulkan device or surface creation (I don't remember which one) to fail.
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "plague", NULL, NULL);
+	if (window == NULL) {
+		fprintf(stderr, "failed to create window\n");
+		return 1;
+	}
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
